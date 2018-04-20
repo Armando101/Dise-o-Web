2625bc1ad829088768267caa40b4b679432706c1
Armando <rivera.armando997@gmail.com>
2018-04-20 04:57:21 -0500

    Ultimo ejercicio

---
+++ Semana_1/Jueves/Tareas/tipos.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+datalist = [1452, 11.23, 1 + 2j, True, 'w3resource', (0, -1), [5, 12], {"clase": 'V', 'section': 'A'} ]
+for x in range(0,len(datalist)):
+	print(datalist[x])
+	print(type(datalist[x]))
